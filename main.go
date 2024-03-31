4b0ef36829b7351fda2c414e9dd112acdc4b04ab
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-03-31 03:56:42 +0200

    continue

--- main.go
+++ main.go
@@ -2,35 +2,16 @@ package main
 
 import (
 	"fmt"
-	//  "strings"
-	"sort"
+	
 )
 
 func main() {
 
-	// greeting := "hello there friends!"
-
-	// fmt.Println(strings.Contains(greeting,"hello!"))
-	// fmt.Println(strings.ReplaceAll(greeting,"hello", "hola"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "re")) //position coutin from 0
-	// fmt.Println(strings.Split(greeting, " "))
+			x := 0
+			for x < 5 {
+				fmt.Println("Value of x is: ", x)
+				x++
+			}
 	
-	//the original value remains unchanged
-	// fmt.Println("original string value =", greeting)
 
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-	
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 36)
-	fmt.Println(index)
-
-	names := []string{"yoshiu", "mario", "bowser", "peach", "luigi"}
-
-	sort.Strings(names)
-	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "bowser"))
 }

602e7163e35215dc4e0c02fa805b7109ea4f5211
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-04-03 01:04:33 +0200

    lesson8

--- main.go
+++ main.go
@@ -1,29 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
+func getInitials(n string) {
 
-func sayGreeting(n string) {
-	fmt.Printf("Good Morning %v \n", n)
 }
-
-func sayBye(n string) {
-	fmt.Printf("Goodbye %v \n", n)
-}
-func cycleNames(n []string, f func(string)) {
-
-	for _, v := range n {
-		f(v)
-	}
-}
-
 func main() {
 
-	sayGreeting("mario")
-	sayGreeting("luigi")
-	sayBye("mario")
-
-	//this function calls a function for every strin iterated in the first arg 
-	cycleNames([]string{"cloud","tifa", "barret"}, sayGreeting)
 }
---
+++ test7.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+	fmt.Printf("Good Morning %v \n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+func cycleNames(n []string, f func(string)) {
+
+	for _, v := range n {
+		f(v)
+	}
+}
+
+//area of a circle
+func circleArea(r float64) float64{
+	
+	return math.Pi * r * r
+}
+ 
+func main7() {
+
+	// sayGreeting("mario")
+	// sayGreeting("luigi")
+	// sayBye("mario")
+
+	
+
+	//this function calls a function for every strin iterated in the first arg
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+	
+	fmt.Println(a1, a2)
+	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+	
+
+
+
+
+}

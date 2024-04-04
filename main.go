2df79d4d5feab751542d4bc760f31f70bf1b2500
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-04-04 16:24:37 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,39 +1,5 @@
 package main
 
-import "fmt"
-
-func updateName(x *string) { //using an asterix in a variable inside a function inside while accepting a pointer as the argument of that function
-	*x = "wedge"
-}
-
 func main() {
 
-	name := "tifa"
-
-	// updateName(name)
-
-	fmt.Println("memory address of name is :", &name)
-
-	m := &name //using & before the var name give syou the pointer (when you make a copy of the original variable)
-	// fmt.Println("memory address:", m)           //here you can ndisplay the pointer
-	// fmt.Println("value at memory address:", *m) //by adding a * before the variable name you can get the value the pointer redirects to
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name) //pointers have a reverse logic to it, by using pointers in the function it makes the variale return to its original value.
-
 }
-
-/*
-
-|---name---|----m-----|
-|   0x001  |   0x002  |
-|---name---|----m-----|
-|  "tifa"  |  p0x001  |
-|----------|----------|
-
-
-
-
-
-*/

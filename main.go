08c871043f3dcefffca9110a8922e38ab8c69169
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-04-07 03:22:39 +0200

    lesson18finished

--- main.go
+++ main.go
@@ -7,13 +7,23 @@ import (
 	"strings"
 )
 
+// User Input lesson18 https://www.youtube.com/watch?v=20HlPtQuc_g
+
+//the package bufio gives the ability to read what the user types in the console
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Println(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+
+}
+
 func createBill() bill {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Create a new bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
@@ -22,10 +32,18 @@ func createBill() bill {
 
 }
 
+func promptOptions(b bill) {
+
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+
+}
+
 func main() {
 
 	mybill := createBill()
-
-	fmt.Println(mybill)
+	promptOptions(mybill)
+	// fmt.Println(mybill)
 
 }
